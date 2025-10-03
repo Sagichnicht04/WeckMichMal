d32bb9aae2380dea4c139f59ecd11adeae633b9d
sagichnicht04 <github@fzuerner.com>
2025-10-03 18:57:32 +0200

    Vertical Scroll in settings

--- app/src/main/java/de/heinzenburger/g2_weckmichmal/ui/screens/Settings.kt
+++ app/src/main/java/de/heinzenburger/g2_weckmichmal/ui/screens/Settings.kt
@@ -12,6 +12,8 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Text
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
@@ -155,9 +157,12 @@ class SettingsScreen : ComponentActivity() {
             )
             Column(Modifier
                 .background(MaterialTheme.colorScheme.background)
-                .fillMaxSize(),
+                .fillMaxSize()
+                .verticalScroll(rememberScrollState())
+                ,
                 verticalArrangement = Arrangement.Center,
                 horizontalAlignment = Alignment.CenterHorizontally
+
             ){
 
                 Button(
